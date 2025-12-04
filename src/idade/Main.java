1796f4f3eefc8029b7af36dd6f0d02d9616a65da
Fabio Cunha Borges <fabiocborges1@gmail.com>
2025-12-04 13:25:05 -0300

    Adicionado sistema de classificacao por idade
    The program prompts the user for their age and categorizes them as 'Criança', 'Adolescente', 'Adulto', or 'Idoso' based on the input.

---
+++ src/idade/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade < 12) {
+            System.out.println("Criança");
+        } else if (idade <= 17) {
+            System.out.println("Adolescente");
+        } else if (idade <= 59) {
+            System.out.println("Adulto");
+        } else {
+            System.out.println("Idoso");
+        }
+
+        scanner.close();
+    }
+}
